3bc4167fcf76f7b4156a2d9ea2e62d4c57c9c5ae
DennisChung <c10381@gmail.com>
2023-12-20 11:47:52 +0800

    [feat] init

---
+++ src/main/java/com/c10381/azuredevpipelineproject/AzureDevPipelineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.c10381.azuredevpipelineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDevPipelineProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AzureDevPipelineProjectApplication.class, args);
+  }
+
+}
